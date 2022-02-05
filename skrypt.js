4541d536b20f4c940e3b979486c359bef3f61aa1
G_Dychala <g.dychala@gmail.com>
2022-02-05 19:26:03 +0100

    add edit hours of work

--- skrypt.js
+++ skrypt.js
@@ -1,5 +1,10 @@
 function gen()
 {
+
+
+
+
+
   
 const text=document.getElementById("edit").value; 
 
@@ -67,7 +72,7 @@ switch(miesiac){
 
 
 document .getElementById ("wybor").innerHTML ="";
-document.getElementById("nag").innerHTML+="<p class='f1'>"+rok+"    "+miesiac+"</p>"+"<button id='druk' onclick=window.print()>🖨️ Drukuj</>";
+document.getElementById("nag").innerHTML+="<p class='f1'>"+rok+"    "+miesiac+"</p>"+"<p id='but'><button id='zap' onclick=zapisz()>👌 Zapisz</></p>";
 if(miesiac=="luty"&&rok%400==0){
     l=29
 }
@@ -93,18 +98,34 @@ else if(miesiac=="luty"&&rok%4!=0){
 for(i=1; i<=l; i++){
  
        if(i==niedziela||i==niedziela+7||i==niedziela+14||i==niedziela+21||i==niedziela+28||i==niedziela+35||i==1&&(miesiac=="styczeń"||miesiac=="maj"||miesiac=="listopad")||i==6&&miesiac=="styczeń"||i==3&&miesiac=="maj"||i==15&&miesiac=="sierpień"||i==11&&miesiac=="listopad"||i==25&&miesiac=="grudzień"||i==26&&miesiac=="grudzień") {
-        document.getElementById("tabela").innerHTML +="<tr class='suncolor'><td >"+i+"."+"</td><td >"+i+"."+nummie+"."+rok+"</td><td ></td><td > </td><td ></td><td > </td><td > </td></tr>"
+        document.getElementById("tabela").innerHTML +="<tr class='suncolor'><td >"+i+"."+"</td><td >"+i+"."+nummie+"."+rok+`</td><td id=tdod${i}><input id=od${i} type='time' value='07:00'/></td><td id=tddo${i}><input id=do${i} type='time' value='15:00'/></td><td id=s${i}></td><td > </td><td > </td></tr>`
     }    
     
        
   else if(i==sobota||i==sobota+7||i==sobota+14||i==sobota+21||i==sobota+28){
-        document.getElementById("tabela").innerHTML +="<tr class='satcolor'><td style='width:5%'>"+i+"."+"</td><td >"+i+"."+nummie+"."+rok+"</td><td ></td><td ></td><td ></td><td > </td><td > </td></tr>"
+        document.getElementById("tabela").innerHTML +="<tr class='satcolor'><td style='width:5%'>"+i+"."+"</td><td >"+i+"."+nummie+"."+rok+`</td><td id=tdod${i}><input id=od${i} type='time' value='07:00'/></td><td id=tddo${i}><input id=do${i} type='time' value='15:00'/></td><td id=s${i}></td><td > </td><td > </td></tr>`
     }                                                                                                    
     
     else{
-        document.getElementById("tabela").innerHTML +="<tr><td>"+i+"."+"</td><td>"+i+"."+nummie+"."+rok+"</td><td></td><td></td><td></td><td > </td><td > </td></tr>"
+        document.getElementById("tabela").innerHTML +="<tr><td>"+i+"."+"</td><td>"+i+"."+nummie+"."+rok+`</td><td id=tdod${i}><input id=od${i} type="time" value='07:00'/></td><td id=tddo${i}><input id=do${i} type="time" value='15:00'/></td><td id=s${i}></td><td > </td><td > </td></tr>`
         }
     
      }
      document.getElementById("tabela").innerHTML +="<tr><td class='noborder'></td><td class='noborder'></td><td class='noborder'></td><td class='noborder'>"+"RAZEM"+"</td><td ></td><td class='noborder'></td><td class='noborder'></td></tr>"
   }   
+
+  
+  function zapisz(){
+const pracownik=document.getElementById('pracownik').value;
+const stanowisko=document.getElementById('stanowisko').value;
+document.getElementById('but').innerHTML='<button id="druk" onclick=window.print()>🖨️ Drukuj</button>'
+document.getElementById('prac').innerHTML=`PRACOWNIK: ${pracownik}`;
+document.getElementById('stan').innerHTML=`STANOWISKO: ${stanowisko}`;
+let k;
+for(k=1; k<=l; k++){
+   // document.getElementById(`s${k}`).innerHTML=document.getElementById(`do${k}`).value-document.getElementById(`od${k}`).value;
+    document.getElementById(`tdod${k}`).innerHTML=document.getElementById(`od${k}`).value;
+    document.getElementById(`tddo${k}`).innerHTML=document.getElementById(`do${k}`).value;
+    
+}
+  }
